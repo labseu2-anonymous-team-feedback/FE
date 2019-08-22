48b6a55ef82990e7b3271aa26df783422bc5f381
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-08-22 14:22:04 +0100

    create signin component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Signup from './components/auth/signup/Signup';
+import Signin from './components/auth/siginin/Signin';
 
 function App() {
   return (
     <Router>
       <Route exact path="/signup" component={Signup} />
+      <Route exact path="/signin" component={Signin} />
     </Router>
   );
 }
--- src/components/auth/siginin/Signin.jsx
+++ src/components/auth/siginin/Signin.jsx
@@ -1,31 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import StyledSignup from './StyledSignup';
 import GoogleButton from '../../../assets/images/google-button.png';
 import SlackButton from '../../../assets/images/slack-button.png';
+import StyledSignin from './StyledSignin';
 
-export default class Signup extends Component {
+export default class Signin extends Component {
   componentDidMount() {}
 
   render() {
     return (
-      <StyledSignup>
+      <StyledSignin>
         <form
           className="text-center border border-light p-5 w-50 z-depth-1"
           action="#!"
         >
-          <p className="h4 mb-4">Sign Up</p>
-
-          <label htmlFor="username" className="d-flex font-weight-bold">
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            className="form-control mb-4"
-            placeholder="Enter Username"
-          />
+          <p className="h4 mb-4">Sign In</p>
 
           <label htmlFor="email" className="d-flex font-weight-bold">
             Email
@@ -37,39 +27,20 @@ export default class Signup extends Component {
             placeholder="E-mail"
           />
 
-          <div className="d-flex justify-content-between">
-            <div className="passwordContainer">
-              <label htmlFor="password" className="d-flex font-weight-bold">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="form-control mb-4"
-                placeholder="Password"
-              />
-            </div>
-
-            <div className="passwordContainer">
-              <label
-                htmlFor="confirmPassword"
-                className="d-flex font-weight-bold"
-              >
-                Confirm Password
-              </label>
-              <input
-                type="password"
-                id="confirmPassword"
-                className="form-control mb-4"
-                placeholder="Confirm Password"
-              />
-            </div>
-          </div>
+          <label htmlFor="password" className="d-flex font-weight-bold">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="form-control mb-4"
+            placeholder="Password"
+          />
 
           <div className="d-flex justify-content-around" />
 
           <button className="btn btn-info btn-block my-4" type="submit">
-            Sign Up
+            Sign In
           </button>
 
           <div className="dividerContainer">
@@ -97,13 +68,12 @@ export default class Signup extends Component {
           </div>
 
           <p>
-            Already have an account? &nbsp;
-            <Link to="/signin">
-              <u>Sign In</u>
+            <Link to="/forgotpassword">
+              <u>Forgot Password?</u>
             </Link>
           </p>
         </form>
-      </StyledSignup>
+      </StyledSignin>
     );
   }
 }
