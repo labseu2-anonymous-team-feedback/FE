dd409a26960c6128926adb98183f37f1f6129acc
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-08-22 11:46:20 +0100

    creating signup optional login markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Signup from './components/auth/signup/Signup';
 
 function App() {
   return (
     <Router>
-      <Signup />
+      <Route exact path="/signup" component={Signup} />
     </Router>
   );
 }
