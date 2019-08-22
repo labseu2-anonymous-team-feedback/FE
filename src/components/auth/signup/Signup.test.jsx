5e7cd692c170c117d41fec7de0fc22407733e7b3
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-08-22 12:33:30 +0100

    refactor

--- src/components/auth/signup/Signup.test.jsx
+++ src/components/auth/signup/Signup.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Signup from '../components/auth/signup/Signup';
+import Signup from './Signup';
 
 describe('<Login />', () => {
   it('renders the Login component correctly', () => {
