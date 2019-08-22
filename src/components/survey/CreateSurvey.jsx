da6bc872d64aa4570928ab346acb4026d9edab83
Awa Melvine <melvineawa@gmail.com>
2019-08-22 11:50:36 +0100

    Create a reusable section divider styled component

--- src/components/survey/CreateSurvey.jsx
+++ src/components/survey/CreateSurvey.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Divider from '../../styles/Divider';
 
 const Question = styled.div`
-  border: 1px solid green;
+  /* border: 1px solid green; */
 `;
 
 const Questions = styled.div`
-  border: 1px solid blue;
+  /* border: 1px solid blue; */
 `;
 
 class CreateSurvey extends Component {
@@ -24,47 +25,60 @@ class CreateSurvey extends Component {
   render() {
     const { data } = this.state;
     return (
-      <form className="border border-light p-5" action="#!">
-        <h1 className="text-center">Create a Survey</h1>
-
-        <div className="form-group">
-          <label htmlFor="title">Survey Title</label>
-          <input
-            type="text"
-            id="title"
-            className="form-control mb-4"
-            value={data.title}
-            onChange={this.change}
-          />
-        </div>
-
-        <Questions>
-          <h2 className="text-center">Survey Questions</h2>
-          <Question>
+      <div className="container">
+        <div className="col-md">
+          <form className="border border-light p-5" action="#!">
+            <h1 className="text-center">Create a Survey</h1>
+            <Divider size={30} />
             <div className="form-group">
-              <label htmlFor="type">Question type</label>
-              <select className="browser-default custom-select mb-4" id="type">
-                <option value="" disabled>
-                  Choose option
-                </option>
-                <option value="text">Text</option>
-                <option value="rating">Rating</option>
-              </select>
+              <label htmlFor="title">Survey Title</label>
+              <input
+                type="text"
+                id="title"
+                className="form-control mb-4"
+                value={data.title}
+                onChange={this.change}
+              />
             </div>
 
-            <div className="form-group">
-              <label htmlFor="text">Question</label>
-              <textarea className="form-control rounded-0" id="text" rows="3" />
-            </div>
-          </Question>
-        </Questions>
+            <Questions>
+              <h2>Survey Questions</h2>
+              <Divider size={30} />
+              <Question>
+                <div className="form-group">
+                  <label htmlFor="text">Question</label>
+                  <textarea
+                    className="form-control rounded-0"
+                    id="text"
+                    rows="3"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="type">Question type</label>
+                  <select
+                    className="browser-default custom-select mb-4"
+                    id="type"
+                  >
+                    <option value="" disabled>
+                      Choose option
+                    </option>
+                    <option value="text">Text</option>
+                    <option value="rating">Rating</option>
+                  </select>
+                </div>
+              </Question>
+            </Questions>
 
-        <div className="form-group">
-          <button className="btn btn-info btn-block" type="submit">
-            Save Survey
-          </button>
+            <Divider size={30} />
+
+            <div className="form-group">
+              <button className="btn btn-info btn-block" type="submit">
+                Save Survey
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     );
   }
 }
