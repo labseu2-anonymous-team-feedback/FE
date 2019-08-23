22240dadcc1adef1d4aba333972efb38570d6133
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-08-23 11:42:13 +0100

    integrate signup component with API

--- src/components/auth/signup/Signup.jsx
+++ src/components/auth/signup/Signup.jsx
@@ -1,113 +1,133 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useMutation } from 'react-apollo';
 
+import ADD_TODO from '../../../graphql/mutations';
 import StyledSignup from './StyledSignup';
 import GoogleButton from '../../../assets/images/google-button.png';
 import SlackButton from '../../../assets/images/slack-button.png';
 
-export default class Signup extends Component {
-  componentDidMount() {}
+export default function Signup() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-  render() {
-    return (
-      <StyledSignup>
-        <form
-          className="text-center border border-light p-5 z-depth-1"
-          action="#!"
-        >
-          <p className="h4 mb-4">Sign Up</p>
+  const [createAccount, { loading, data, error }] = useMutation(ADD_TODO);
+  // {(createAccount, { loading, data, error }) => (
+  console.log(loading);
+  console.log(data);
+  console.log(error);
+  return (
+    <StyledSignup>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          createAccount({ variables: { username, email, password } });
+        }}
+        className="text-center border border-light p-5 z-depth-1"
+        action="#!"
+      >
+        <p className="h4 mb-4">Sign Up</p>
 
-          <label htmlFor="username" className="d-flex font-weight-bold">
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            className="form-control mb-4"
-            placeholder="Enter Username"
-            required
-          />
+        <label htmlFor="username" className="d-flex font-weight-bold">
+          Username
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="form-control mb-4"
+          placeholder="Enter Username"
+          name="username"
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
 
-          <label htmlFor="email" className="d-flex font-weight-bold">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="form-control mb-4"
-            placeholder="E-mail"
-            required
-          />
+        <label htmlFor="email" className="d-flex font-weight-bold">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="form-control mb-4"
+          placeholder="E-mail"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
 
-          <div className="d-flex justify-content-between passwordContainerDiv">
-            <div className="passwordContainer">
-              <label htmlFor="password" className="d-flex font-weight-bold">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="form-control mb-4"
-                placeholder="Password"
-                required
-              />
-            </div>
-
-            <div className="passwordContainer">
-              <label
-                htmlFor="confirmPassword"
-                className="d-flex font-weight-bold"
-              >
-                Confirm Password
-              </label>
-              <input
-                type="password"
-                id="confirmPassword"
-                className="form-control mb-4"
-                placeholder="Confirm Password"
-                required
-              />
-            </div>
+        <div className="d-flex justify-content-between passwordContainerDiv">
+          <div className="passwordContainer">
+            <label htmlFor="password" className="d-flex font-weight-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="form-control mb-4"
+              placeholder="Password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
 
-          <div className="d-flex justify-content-around" />
+          <div className="passwordContainer">
+            <label
+              htmlFor="confirmPassword"
+              className="d-flex font-weight-bold"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              className="form-control mb-4"
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </div>
+        </div>
 
-          <button className="btn btn-info btn-block my-4" type="submit">
-            Sign Up
-          </button>
+        <div className="d-flex justify-content-around" />
 
-          <div className="dividerContainer">
-            <div className="divider">
-              <hr />
-            </div>
-            <p className="font-weight-bold text-muted">OR</p>
-            <div className="divider">
-              <hr />
-            </div>
+        <button className="btn btn-info btn-block my-4" type="submit">
+          Sign Up
+        </button>
+
+        <div className="dividerContainer">
+          <div className="divider">
+            <hr />
+          </div>
+          <p className="font-weight-bold text-muted">OR</p>
+          <div className="divider">
+            <hr />
+          </div>
+        </div>
+
+        <div className="d-flex optionalLoginContainer">
+          <div className="optional-login">
+            <a href="##">
+              <img src={GoogleButton} alt="Sign up with google" />
+            </a>
           </div>
 
-          <div className="d-flex optionalLoginContainer">
-            <div className="optional-login">
-              <a href="##">
-                <img src={GoogleButton} alt="Sign up with google" />
-              </a>
-            </div>
-
-            <div className="optional-login">
-              <a href="##">
-                <img src={SlackButton} alt="Sign up with slack" />
-              </a>
-            </div>
+          <div className="optional-login">
+            <a href="##">
+              <img src={SlackButton} alt="Sign up with slack" />
+            </a>
           </div>
+        </div>
 
-          <p>
-            Already have an account? &nbsp;
-            <Link to="/signin">
-              <u>Sign In</u>
-            </Link>
-          </p>
-        </form>
-      </StyledSignup>
-    );
-  }
+        <p>
+          Already have an account? &nbsp;
+          <Link to="/signin">
+            <u>Sign In</u>
+          </Link>
+        </p>
+      </form>
+    </StyledSignup>
+  );
 }
