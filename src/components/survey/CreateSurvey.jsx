67bc8ae0b3c83bcf326a60485f278dbac54b4eca
Awa Melvine <melvineawa@gmail.com>
2019-08-26 21:37:41 +0100

    feat(survey): Implement create survey mutation on client

--- src/components/survey/CreateSurvey.jsx
+++ src/components/survey/CreateSurvey.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
 import Divider from '../../styles/Divider';
 import Question from './Question';
 import { AddButton, Container } from './SurveyStyles';
+
+import { CREATE_NEW_SURVEY } from '../../graphql/mutations';
 
 class CreateSurvey extends Component {
   constructor(props) {
@@ -49,63 +52,80 @@ class CreateSurvey extends Component {
     return (
       <Container className="container">
         <div className="col-md">
-          <form className="border border-light p-5" action="#!">
-            <h1 className="text-center">Create a Survey</h1>
-            <Divider size={30} />
-            <div className="form-group">
-              <label htmlFor="title">Survey Title</label>
-              <input
-                type="text"
-                id="title"
-                className="form-control mb-4"
-                name="title"
-                value={title}
-                onChange={this.handleChangeSurvey}
-              />
-            </div>
+          <Mutation mutation={CREATE_NEW_SURVEY}>
+            {(createNewSurvey) => (
+              <form
+                className="border border-light p-5"
+                action="#!"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  if (!title) {
+                    // TO DO: Validate survey title
+                  } else {
+                    createNewSurvey({
+                      variables: { title },
+                    });
+                  }
+                }}
+              >
+                <h1 className="text-center">Create a Survey</h1>
+                <Divider size={30} />
+                <div className="form-group">
+                  <label htmlFor="title">Survey Title</label>
+                  <input
+                    type="text"
+                    id="title"
+                    className="form-control mb-4"
+                    name="title"
+                    value={title}
+                    onChange={this.handleChangeSurvey}
+                  />
+                </div>
 
-            <div>
-              <h2>Survey Questions</h2>
-              <Divider size={30} />
+                <div>
+                  <h2>Survey Questions</h2>
+                  <Divider size={30} />
 
-              {questions.map((question, index) => (
-                <Question
-                  key={index.toString()}
-                  text={question.text}
-                  type={question.type}
-                  index={index}
-                  handleChangeQuestion={this.handleChangeQuestion}
-                  removeQuestion={this.removeQuestion}
-                />
-              ))}
+                  {questions.map((question, index) => (
+                    <Question
+                      key={index.toString()}
+                      text={question.text}
+                      type={question.type}
+                      index={index}
+                      handleChangeQuestion={this.handleChangeQuestion}
+                      removeQuestion={this.removeQuestion}
+                    />
+                  ))}
 
-              <div className="text-center">
-                <AddButton
-                  type="button"
-                  className="btn btn-light"
-                  onClick={() => {
-                    this.setState((prev) => ({
-                      ...prev,
-                      questions: prev.questions.concat({
-                        text: '',
-                        type: '',
-                      }),
-                    }));
-                  }}
-                >
-                  +
-                </AddButton>
-              </div>
-            </div>
+                  <div className="text-center">
+                    <AddButton
+                      type="button"
+                      className="btn btn-light"
+                      onClick={() => {
+                        this.setState((prev) => ({
+                          ...prev,
+                          questions: prev.questions.concat({
+                            text: '',
+                            type: '',
+                          }),
+                        }));
+                      }}
+                    >
+                      +
+                    </AddButton>
+                  </div>
+                </div>
 
-            <Divider size={30} />
+                <Divider size={30} />
 
-            <div className="form-group">
-              <button className="btn btn-info btn-block" type="submit">
-                Save Survey
-              </button>
-            </div>
-          </form>
+                <div className="form-group">
+                  <button className="btn btn-info btn-block" type="submit">
+                    Save Survey
+                  </button>
+                </div>
+              </form>
+            )}
+          </Mutation>
         </div>
       </Container>
     );
--- src/index.jsx
+++ src/index.jsx
@@ -23,7 +23,7 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: token ? `${token}` : '',
+      authorization: token ? `${token}` : null,
     },
   };
 });
