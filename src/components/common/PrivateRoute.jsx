87e6b08f42540f857a2625e2c4c92f16f3321415
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-08-27 14:59:53 +0100

    create util function

--- src/components/common/PrivateRoute.jsx
+++ src/components/common/PrivateRoute.jsx
@@ -1,22 +1,12 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import jwtDecode from "jwt-decode";
-
-import PropTypes from "prop-types";
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { isLoggedIn } from '../../utils';
 
 export const PrivateRoute = ({ component: Component, ...rest }) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    const decodedToken = jwtDecode(token);
-    console.log(decodedToken);
-
-    const currentTime = Date.now() / 1000;
-    console.log(decodedToken.exp < currentTime);
-    if (decodedToken.exp < currentTime) {
-      // Logout user
-      return <Redirect to="login" />;
-    }
-    return <Route {...rest} render={props => <Component {...props} />} />;
+  const isSignedIn = isLoggedIn();
+  if (isSignedIn) {
+    return <Route {...rest} render={(props) => <Component {...props} />} />;
   }
 
   return <Redirect to="login" />;
@@ -25,8 +15,7 @@ export const PrivateRoute = ({ component: Component, ...rest }) => {
 // );
 
 PrivateRoute.propTypes = {
-  exact: PropTypes.bool
+  exact: PropTypes.bool,
 };
-
 
 export default PrivateRoute;
