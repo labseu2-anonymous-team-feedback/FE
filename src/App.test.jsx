ca6f91042e08e4ac9cf7f699871474dbb2b85d36
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-08-27 15:35:19 +0100

    refactor

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<Signin />', () => {
+  it('renders the App component correctly', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
