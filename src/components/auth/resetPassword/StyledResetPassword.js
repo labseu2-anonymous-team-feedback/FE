3d350bb7fdd9e5e1161d20c9cc73d30fefd51680
nanoblit <nanoblit@users.noreply.github.com>
2019-08-28 15:22:40 +0200

    Add reset password and resed password confirmation pages

---
+++ src/components/auth/resetPassword/StyledResetPassword.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const ResetPasswordDiv = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 1rem;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  form, .confirmContainer {
+    width: 50%;
+    @media (max-width: 1400px) {
+      width: 60%;
+    }
+
+    @media (max-width: 1200px) {
+      width: 70%;
+    }
+
+    @media (max-width: 1050px) {
+      width: 85%;
+    }
+
+    @media (max-width: 870px) {
+      width: 95%;
+    }
+  }
+`;
+
+export default ResetPasswordDiv;
