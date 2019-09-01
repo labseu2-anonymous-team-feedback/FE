1dcc7bbc242a5fe0da88eed0e9d75a89580bd055
Awa Melvine <melvineawa@gmail.com>
2019-09-01 18:56:28 +0100

    feat(password): Submit reset password email

--- src/components/auth/resetPassword/ResetPassword.jsx
+++ src/components/auth/resetPassword/ResetPassword.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Mutation } from 'react-apollo';
 import { toast } from 'react-toastify'; // eslint-disable-line
 import ResetPasswordDiv from './StyledResetPassword';
 import TextInput from '../../common/TextInput';
@@ -7,43 +8,54 @@ import { SEND_RESET_PASSWORD_EMAIL } from '../../../graphql/mutations';
 function ResetPassword() {
   const [email, setEmail] = useState('');
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    console.log(email);
-    setEmail('');
-  };
-
   const onChange = (e) => setEmail(e.target.value);
 
   return (
-    <ResetPasswordDiv>
-      <form
-        className="text-center border border-light p-5 z-depth-1"
-        action="#!"
-        onSubmit={onSubmit}
-      >
-        <p className="h4 mb-4">Reset Password</p>
-        <p>
-          Enter your email in the input below and we will help you recover your
-          password.
-        </p>
+    <Mutation mutation={SEND_RESET_PASSWORD_EMAIL}>
+      {(sendResetPasswordEmail) => (
+        <ResetPasswordDiv>
+          <form
+            className="text-center border border-light p-5 z-depth-1"
+            action="#!"
+            onSubmit={(e) => {
+              e.preventDefault();
+              sendResetPasswordEmail({
+                variables: {
+                  email,
+                },
+              })
+                .then((msg) => {
+                  console.log('Email sent: ', msg);
+                })
+                .catch((err) => {
+                  console.log('Failed to sent: ', err);
+                });
+            }}
+          >
+            <p className="h4 mb-4">Reset Password</p>
+            <p>
+              Enter your email in the input below and we will help you recover
+              your password.
+            </p>
 
-        <TextInput
-          title="Email"
-          name="email"
-          type="email"
-          value={email}
-          onChange={onChange}
-          id="email"
-          className="form-control mb-4"
-          required
-        />
+            <TextInput
+              title="Email"
+              name="email"
+              type="email"
+              value={email}
+              onChange={onChange}
+              id="email"
+              className="form-control mb-4"
+              required
+            />
 
-        <button className="btn btn-info btn-block my-4" type="submit">
-          Reset Password
-        </button>
-      </form>
-    </ResetPasswordDiv>
+            <button className="btn btn-info btn-block my-4" type="submit">
+              Reset Password
+            </button>
+          </form>
+        </ResetPasswordDiv>
+      )}
+    </Mutation>
   );
 }
 
