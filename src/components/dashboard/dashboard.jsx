56fcbae13014e6ced79b7b39f3396365d03bb93d
Petar Scepanovic <hiborija@gmail.com>
2019-09-05 13:29:47 +0200

    installed appolo/react-hooks and grapgql-tag for apollo client, sucessfully query for fetching surveys

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import qql from 'graphql-tag';
+import { GET_SURVEYS } from '../../graphql/queries';
 
-export class Dashboard extends Component {
-  render() {
+export function Dashboard() {
+  const { data, loading, error } = useQuery(GET_SURVEYS);
+  if (data.getUserSurveys) {
     return (
       <div>
-        <p>Hello</p>
+        {data.getUserSurveys.map((survey) => (
+          <p key={survey.title}>{survey.title}</p>
+        ))}
       </div>
     );
   }
+  return <div>Loading</div>
 }
 
 export default Dashboard;
