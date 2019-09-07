557ee3400840013aee334c0fb1c721eafe33e8ce
PascalUlor <pascalulor@yahoo.com>
2019-09-07 18:23:31 +0100

    seed data into result component

--- src/components/response/Response.jsx
+++ src/components/response/Response.jsx
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 import { Container, MainContainer } from './ResponseStyles';
 import SideBar from '../common/Sidebar';
+import Divider from '../../styles/Divider';
+import ResultSort from './ResultSort';
 
 
 class Response extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selectedSurvey: {},
+      answers: {},
+    };
+  }
 
-    this.state = {};
+  componentDidMount() {
+    Promise.all([
+      fetch(`${process.env.PUBLIC_URL || ''}/survey.json`),
+      fetch(`${process.env.PUBLIC_URL || ''}/feedback.json`),
+    ])
+      .then((responses) => Promise.all(responses.map((resp) => resp.json())))
+      .then(([survey, feedback]) => {
+        const surveyFeedback = feedback.data.getSurveyFeedback;
+        this.setState({ answers: surveyFeedback });
+        const userSurvey = survey.data.getUserSurveys;
+        this.setState({ selectedSurvey: userSurvey[0] });
+      });
   }
 
   render() {
+    const { answers } = this.state;
+    const { selectedSurvey } = this.state;
+    const { title, questions } = selectedSurvey;
     return (
       <MainContainer>
         <SideBar />
         <Container className="container">
           <h1 className="text-center create-survey-title f-1">
-            Response Title
+            {title}
           </h1>
-
+          <Divider size={30} />
+          {questions && answers !== {} && <ResultSort question={questions} answers={answers} />}
         </Container>
       </MainContainer>
 
---
+++ src/components/response/ResultSort.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+
+class ResultSort extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { answers } = this.props;
+    const { question } = this.props;
+    const processedData = (arr1, arr2) => {
+      const sortData = [];
+      arr1.map((ques) => arr2.map((ans) => {
+        if (ques.id === ans.questionId) {
+          sortData.push({
+            ...ques,
+            surveyId: ans.surveyId,
+            questionId: ques.id,
+            answerId: ans.id,
+            comment: ans.comment,
+            rating: ans.rating,
+          });
+        }
+      }));
+      return sortData;
+    };
+
+    const sortedAnswers = processedData(question, answers);
+    return (
+      sortedAnswers.map((ans) => (
+        <div key={ans.answerId}>
+          {ans.comment}
+          {ans.rating}
+        </div>
+      ))
+    );
+  }
+}
+
+export default ResultSort;
