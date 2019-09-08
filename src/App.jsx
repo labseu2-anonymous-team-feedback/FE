bd8e2a897c278918a7b071f3e2538d1a4363f579
Petar Scepanovic <hiborija@gmail.com>
2019-09-08 18:35:53 +0200

    implemented code splitting for one of the components userDashboard in app.js, its a practice of only loading the Javascript the moment when you need it. It can improve the overall performance of the app. I used React.lazy and Suspense. So basically itwill load a dependency when needed

--- src/App.jsx
+++ src/App.jsx
@@ -10,34 +10,42 @@ import ResetPassword from './components/auth/resetPassword/ResetPassword';
 import ResetPasswordConfirmation from './components/auth/resetPassword/ResetPasswordConfirmation';
 import PrivateRoute from './components/common/PrivateRoute';
 import NewPassword from './components/auth/resetPassword/NewPassword';
-import userDashboard from './components/dashboard/Dashboard';
+// import userDashboard from './components/dashboard/Dashboard';
+
+const userDashboard = React.lazy(() => import('./components/dashboard/Dashboard'));
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={Navigation} />
-      <Route
-        exact
-        path="/verify_account/:verifyToken"
-        component={Navigation}
-      />
-      <Route exact path="/register" component={Signup} />
-      <Route exact path="/login" component={Signin} />
-      <Route exact path="/resetPassword" component={ResetPassword} />
-      <Route
-        exact
-        path="/resetPasswordConfirmation"
-        component={ResetPasswordConfirmation}
-      />
-      <Route
-        exact
-        path="/password_reset/:resetToken"
-        component={NewPassword}
-      />
-      <Switch>
-        <PrivateRoute exact path="/" component={CreateSurvey} />
-      </Switch>
-      <Route exact path="/dashboard" component={userDashboard} />
+      <React.Suspense fallback={<p>Please wait</p>}>
+        <Route exact path="/" component={Navigation} />
+        <Route
+          exact
+          path="/verify_account/:verifyToken"
+          component={Navigation}
+        />
+        <Route exact path="/register" component={Signup} />
+        <Route exact path="/login" component={Signin} />
+        <Route
+          exact
+          path="/resetPassword"
+          component={ResetPassword}
+        />
+        <Route
+          exact
+          path="/resetPasswordConfirmation"
+          component={ResetPasswordConfirmation}
+        />
+        <Route
+          exact
+          path="/password_reset/:resetToken"
+          component={NewPassword}
+        />
+        <Switch>
+          <PrivateRoute exact path="/" component={CreateSurvey} />
+        </Switch>
+        <Route exact path="/dashboard" component={userDashboard} />
+      </React.Suspense>
     </Router>
   );
 }
