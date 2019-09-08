33dc4a04c9895b074160a0122e9dae26bd99b829
Petar Scepanovic <hiborija@gmail.com>
2019-09-08 18:41:44 +0200

    minor change

--- src/components/dashboard/DashboardStyles.js
+++ src/components/dashboard/DashboardStyles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const DashboardContainer = styled.div`
+export const DashboardContainer = styled.section`
   margin: 0 auto;
   display: flex;
   background: #ececec;
