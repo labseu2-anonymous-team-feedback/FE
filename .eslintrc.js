cd3420a98d0f79058217621392ba3530cb50ddf2
Petar Scepanovic <hiborija@gmail.com>
2019-09-09 11:09:39 +0200

    refactored app.js, added React lazy loading for components

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     "import/prefer-default-export": 0,
     "linebreak-style": 0,
     "react/prefer-stateless-function": 0,
+    "implicit-arrow-linebreak":0,
     "jsx-a11y/label-has-associated-control": [
       "error",
       {
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import { withApollo } from 'react-apollo';
 
-import Signup from './components/auth/signup/Signup';
-import Signin from './components/auth/siginin/Signin';
 import Navigation from './components/common/Navigation';
-import CreateSurvey from './components/survey/CreateSurvey';
-import ResetPassword from './components/auth/resetPassword/ResetPassword';
-import ResetPasswordConfirmation from './components/auth/resetPassword/ResetPasswordConfirmation';
 import PrivateRoute from './components/common/PrivateRoute';
-import NewPassword from './components/auth/resetPassword/NewPassword';
-// import userDashboard from './components/dashboard/Dashboard';
 
-const userDashboard = React.lazy(() => import('./components/dashboard/Dashboard'));
+const Signup = React.lazy(() =>
+  import('./components/auth/signup/Signup'));
+
+const Signin = React.lazy(() =>
+  import('./components/auth/siginin/Signin'));
+
+const userDashboard = React.lazy(() =>
+  import('./components/dashboard/Dashboard'));
+
+const CreateSurvey = React.lazy(() =>
+  import('./components/survey/CreateSurvey'));
+
+const ResetPassword = React.lazy(() =>
+  import('./components/auth/resetPassword/ResetPassword'));
+
+const ResetPasswordConfirmation = React.lazy(() =>
+  import('./components/auth/resetPassword/ResetPasswordConfirmation'));
+
+const NewPassword = React.lazy(() =>
+  import('./components/auth/resetPassword/NewPassword'));
 
 function App() {
   return (
