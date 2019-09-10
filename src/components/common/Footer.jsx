1e1d81be51d394ac9561555a796c3190e88bfead
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-09-10 11:34:26 +0100

    change footer color

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,57 +3,17 @@ import styled from 'styled-components';
 import { isLoggedIn } from '../../utils';
 
 const StyledFooter = styled.footer`
-  background-color: #63beff;
+  background-color: #d9eaf7;
+  color: #63beff;
 `;
 
 export default function Footer() {
   const isSignedIn = isLoggedIn();
   if (isSignedIn) {
     return (
-      <StyledFooter className="page-footer font-small darken-3">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 py-5">
-              <div className=" flex-center">
-                <a className="fb-ic" href="##">
-                  <i className="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x">
-                    {' '}
-                  </i>
-                </a>
-
-                <a className="tw-ic" href="##">
-                  <i className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x">
-                    {' '}
-                  </i>
-                </a>
-
-                <a className="gplus-ic" href="##">
-                  <i className="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x">
-                    {' '}
-                  </i>
-                </a>
-
-                <a className="li-ic" href="##">
-                  <i className="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x">
-                    {' '}
-                  </i>
-                </a>
-
-                <a className="ins-ic" href="##">
-                  <i className="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x">
-                    {' '}
-                  </i>
-                </a>
-
-                <a className="pin-ic" href="##">
-                  <i className="fab fa-pinterest fa-lg white-text fa-2x"> </i>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+      <StyledFooter className="font-small darken-3">
         <div className="footer-copyright text-center py-3">
-          ©
+          © 
           {' '}
           {new Date().getFullYear()}
           {' '}
