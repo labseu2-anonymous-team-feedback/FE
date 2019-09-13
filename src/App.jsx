8c3cd5beaf8cec728daba0b6ae94419b79ed1a42
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-09-13 16:06:13 +0100

    fix indent issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,37 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { withApollo } from "react-apollo";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { withApollo } from 'react-apollo';
 
-import Navigation from "./components/common/Navigation";
-import PrivateRoute from "./components/common/PrivateRoute";
-import Footer from "./components/common/Footer";
-import Spinner from "./components/common/Spinner";
-import FeedbackResponse from "./components/feedback/Feedback";
-import Success from "./components/common/success/Success";
-import Response from "./components/response/Response";
-import DashboardLayout from "./components/common/layouts/DashboardLayout";
-import NotFound from "./components/common/error/NotFound";
+import Navigation from './components/common/Navigation';
+import PrivateRoute from './components/common/PrivateRoute';
+import Footer from './components/common/Footer';
+import Spinner from './components/common/Spinner';
+import FeedbackResponse from './components/feedback/Feedback';
+import Success from './components/common/success/Success';
+import Response from './components/response/Response';
+import DashboardLayout from './components/common/layouts/DashboardLayout';
+import NotFound from './components/common/error/NotFound';
 
-const Signup = React.lazy(() => import("./components/auth/signup/Signup"));
+const Signup = React.lazy(() => import('./components/auth/signup/Signup'));
 
-const Signin = React.lazy(() => import("./components/auth/siginin/Signin"));
+const Signin = React.lazy(() => import('./components/auth/siginin/Signin'));
 
 const userDashboard = React.lazy(() =>
-  import("./components/dashboard/Dashboard")
-);
+  import('./components/dashboard/Dashboard'),);
 
 const CreateSurvey = React.lazy(() =>
-  import("./components/survey/CreateSurvey")
-);
+  import('./components/survey/CreateSurvey'),);
 
-const Survey = React.lazy(() => import("./components/survey/Survey"));
+const Survey = React.lazy(() => import('./components/survey/Survey'));
 
 const ResetPassword = React.lazy(() =>
-  import("./components/auth/resetPassword/ResetPassword")
-);
+  import('./components/auth/resetPassword/ResetPassword'),);
 
 const ResetPasswordConfirmation = React.lazy(() =>
-  import("./components/auth/resetPassword/ResetPasswordConfirmation")
-);
+  import('./components/auth/resetPassword/ResetPasswordConfirmation'),);
 
 const NewPassword = React.lazy(() =>
-  import("./components/auth/resetPassword/NewPassword")
-);
+  import('./components/auth/resetPassword/NewPassword'),);
 
 function App() {
   return (
