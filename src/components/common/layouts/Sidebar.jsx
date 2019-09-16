7072da08ac4401754f5e9d90dbf9a413ca3d5408
Awa Melvine <melvineawa@gmail.com>
2019-09-16 08:32:27 +0100

    Display logged in username on sidebar

--- src/components/common/layouts/Sidebar.jsx
+++ src/components/common/layouts/Sidebar.jsx
@@ -1,30 +1,53 @@
 import React from 'react';
+import jwtDecode from 'jwt-decode';
 import ProfileImage from '../ProfileImage';
 import SideNavItem from './SideNavItem';
 import { sidebarIcons, sidebarTexts } from '../../../assets/icons/icons';
 import { StyledSidebar } from './DashboardLayoutStyles';
 
-const SidebarContainer = () => (
-  <StyledSidebar>
-    <ProfileImage
-      name="Bob Stone"
-      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1m1DOmfCTW5yaJjjehDNcrTnJlFaph-ZADDYsCsyzwDJWS_6z"
-    />
-    <div className="nav-items">
-      <SideNavItem
-        text={sidebarTexts.create_survey}
-        icon={sidebarIcons.create_survey}
-        path="/create_survey"
-      />
+class SidebarContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+  }
 
-      <SideNavItem
-        text={sidebarTexts.settings}
-        icon={sidebarIcons.settings}
-        path="/profile"
-      />
-    </div>
-    <div className="empty-div" />
-  </StyledSidebar>
-);
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const user = jwtDecode(token);
+      this.setState({ user });
+    } else {
+      this.setState({ user: null });
+    }
+  }
+
+  render() {
+    const { user } = this.state;
+    return (
+      <StyledSidebar>
+        <ProfileImage
+          name={user && user.username}
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1m1DOmfCTW5yaJjjehDNcrTnJlFaph-ZADDYsCsyzwDJWS_6z"
+        />
+        <div className="nav-items">
+          <SideNavItem
+            text={sidebarTexts.create_survey}
+            icon={sidebarIcons.create_survey}
+            path="/create_survey"
+          />
+
+          <SideNavItem
+            text={sidebarTexts.settings}
+            icon={sidebarIcons.settings}
+            path="/profile"
+          />
+        </div>
+        <div className="empty-div" />
+      </StyledSidebar>
+    );
+  }
+}
 
 export default SidebarContainer;
