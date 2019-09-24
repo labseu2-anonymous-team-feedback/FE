ea38de1b4d38dd45d910c3451c8efa68f9ba8df8
Orie Chinedu <oriechinedu@gmail.com>
2019-09-24 11:28:05 +0100

    Remove redundant variable

--- src/components/survey/CreateSurvey.jsx
+++ src/components/survey/CreateSurvey.jsx
@@ -4,7 +4,6 @@ import { toast } from 'react-toastify';
 import { Redirect } from 'react-router-dom';
 import propTypes from 'prop-types';
 
-import qql from 'graphql-tag';
 import Divider from '../../styles/Divider';
 import Question from './Question';
 import { AddButton, Container, ButtonGroup } from './SurveyStyles';
