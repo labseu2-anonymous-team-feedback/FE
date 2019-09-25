2391d60be322b07c3a817d3e4a34a22d45490456
Petar Scepanovic <hiborija@gmail.com>
2019-09-25 11:02:14 +0200

    update

--- src/components/dashboard/Survey.jsx
+++ src/components/dashboard/Survey.jsx
@@ -5,61 +5,62 @@ import { Link } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { toast } from 'react-toastify';
 import {
-	StyledSurvey,
-	SurveyButton,
-	StyledSurveyContainer,
+  StyledSurvey,
+  SurveyButton,
+  StyledSurveyContainer,
 } from './DashboardStyles';
 
-const Survey = (props) => {
-	const [copied, setCopied] = useState(false);
-	if (copied) {
-		toast('link has been copied to clipboard', {
-			className: 'toast-success',
-		});
-		setCopied(false);
-	}
+const Survey = props => {
+  const [copied, setCopied] = useState(false);
+  if (copied) {
+    toast('link has been copied to clipboard', {
+      className: 'toast-success',
+    });
+    setCopied(false);
+  }
 
-	const { data } = props;
-	return (
-		<StyledSurveyContainer>
-			{data.length > 0 ? (
-				data.map((survey) => {
-					const url = `${window.location.origin}/take_survey/${survey.id}`;
-					return (
-						<StyledSurvey key={survey.id}>
-							<h2 className="survey-title">{survey.title}</h2>
-							<div className="copy-btn-div">
-								<CopyToClipboard text={url} onCopy={() => setCopied(true)}>
-									<div className="btn-link-wrapper">
-										<i className="fas fa-link"></i>
-										&nbsp;
-										<span>Get invite link</span>
-									</div>
-								</CopyToClipboard>
-							</div>
-							<div className="survey-actions">
-								<div className="questions">
-									<Link to={`/survey/${survey.id}`}>
-										{survey.questions ? survey.questions.length : '0'}{' '}
-										Question(s)
-									</Link>
-								</div>
-								<SurveyButton className="btn" to={`/survey/${survey.id}`}>
-									View Details
-								</SurveyButton>
-							</div>
-						</StyledSurvey>
-					);
-				})
-			) : (
-				<h4>There are no surveys for this user</h4>
-			)}
-		</StyledSurveyContainer>
-	);
+
+  const { data } = props;
+  return (
+    <StyledSurveyContainer>
+      {data.length > 0 ? (
+        data.map(survey => {
+          const url = `${window.location.origin}/take_survey/${survey.id}`;
+          return (
+            <StyledSurvey key={survey.id}>
+              <h2 className="survey-title">{survey.title}</h2>
+              <div className="copy-btn-div">
+                <CopyToClipboard text={url} onCopy={() => setCopied(true)}>
+                  <div className="btn-link-wrapper">
+                    <i className="fas fa-link"></i>
+                    &nbsp;
+                    <span>Get invite link</span>
+                  </div>
+                </CopyToClipboard>
+              </div>
+              <div className="survey-actions">
+                <div className="questions">
+                  <Link to={`/survey/${survey.id}`}>
+                    {survey.questions ? survey.questions.length : '0'}{' '}
+                    Question(s)
+                  </Link>
+                </div>
+                <SurveyButton className="btn" to={`/survey/${survey.id}`}>
+                  View Details
+                </SurveyButton>
+              </div>
+            </StyledSurvey>
+          );
+        })
+      ) : (
+        <h4>There are no surveys for this user</h4>
+      )}
+    </StyledSurveyContainer>
+  );
 };
 
 Survey.propTypes = {
-	data: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)).isRequired,
+  data: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)).isRequired,
 };
 
 export default Survey;
