32c59e092c8b34f62024f18125e456f4641233b6
nanoblit <nanoblit@users.noreply.github.com>
2019-09-27 11:55:32 +0200

    Make landing page responsive

--- src/components/landingpage/MainArea.jsx
+++ src/components/landingpage/MainArea.jsx
@@ -1,72 +1,104 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import {
-  white, smallSpace, black, bodyHero, mediumSpace2, heading1,
-} from '../../styles/variables';
-import Button from '../../styles/Button';
+  white,
+  smallSpace,
+  black,
+  bodyHero,
+  mediumSpace2,
+  heading1
+} from "../../styles/variables";
+import Button from "../../styles/Button";
 
 const MainArea = () => (
   <Wrapper>
     <TextArea>
       <div className="text">
         <h1>Anonymous Team Feedback</h1>
-        <p>Improve team performance by giving and receiving feedback anonymously from team-mates</p>
+        <p>
+          Improve team performance by giving and receiving feedback anonymously
+          from team-mates
+        </p>
       </div>
 
-
-      <div className="buttons-container ">
+      <ButtonsDiv className="buttons-container ">
         <Link to="/register">
-          <Button className="btn btn-block" type="submit">
+          <GetStartedButton className="btn btn-block" type="submit">
             Get Started
-          </Button>
+          </GetStartedButton>
         </Link>
-      </div>
+      </ButtonsDiv>
     </TextArea>
   </Wrapper>
 );
 
+const ButtonsDiv = styled.div`
+  @media (max-width: 450px) {
+    margin: 0 !important;
+    width: 100% !important;
+  }
+`;
+
+const GetStartedButton = styled(Button)`
+@media (max-width: 450px) {
+    width: 100%;
+  }
+`;
 
 const Wrapper = styled.div`
-min-height: 76vh;
-margin: 0 auto;
-max-width: 100%;
-padding: 0 ${smallSpace};
-display: flex;
-align-items: center;
-background: ${white};
+  min-height: 76vh;
+  margin: 0 auto;
+  max-width: 100%;
+  padding: 0 ${smallSpace};
+  display: flex;
+  align-items: center;
+  background: ${white};
 `;
 
 const TextArea = styled.div`
-position: relative;
-margin: 0 auto;
-height: 100%;
-width: 30%;
-font-size: 5rem;
-display: flex;
-flex-direction: column;
-justify-content: center;
+  position: relative;
+  margin: 0 auto;
+  height: 100%;
+  width: 30%;
+  font-size: 5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
-::before {
+  @media (max-width: 1200px) {
+    width: 60%;
+  }
+
+  @media (max-width: 800px) {
+    width: 80%;
+  }
+
+  ::before {
     right: 100%;
-}
-::after {
+  }
+  ::after {
     left: 100%;
-}
+  }
 
-::before, ::after {
+  ::before,
+  ::after {
     content: "";
     display: block;
-    background: url('../../assets/images/mobile.png');
+    background: url("../../assets/images/mobile.png");
     position: absolute;
     border-top: 1px solid #404545;
     width: 6.25rem;
     height: 6rem;
     top: 50%;
     margin: 0 15rem;
-}
 
-h1,
+    @media (max-width: 1200px) {
+      display: none;
+    }
+  }
+
+  h1,
   p {
     color: ${black};
   }
@@ -81,10 +113,8 @@ h1,
     margin-bottom: ${mediumSpace2};
   }
   .text {
-    
     text-align: center;
   }
-
 
   .buttons-container {
     width: 60%;
@@ -94,7 +124,6 @@ h1,
       margin-right: ${smallSpace};
     }
   }
-
 `;
 
 export default MainArea;
