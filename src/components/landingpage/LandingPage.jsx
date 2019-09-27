ddda3590d99d17018d60565db3b8d469e71055fc
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-09-27 10:49:29 +0100

    refactor landing page

--- src/components/landingpage/LandingPage.jsx
+++ src/components/landingpage/LandingPage.jsx
@@ -1,23 +1,40 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react';
 import styled from 'styled-components';
+
 import MainArea from './MainArea';
 import { white } from '../../styles/variables';
 import FeedbackVideo from '../../assets/media/feedback.mp4';
-
+import StyledDetailsArea from './LandingPageStyles';
+import image from '../../assets/images/giving-feedback.png';
 const LandingPage = () => (
   <div>
     <MainArea />
-    <div>
-      <div>
-        <h1>Mee</h1>
+    <StyledDetailsArea
+      style={{
+        backgroundImage: `linear-gradient(to right bottom, rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), url(${image})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div className="details-content">
+        <h3>Signup </h3>
+        <p>Signup to the application using your email or google</p>
+
+        <h3>Create a Survey </h3>
+        <p>Create a survey with a list of questions </p>
+
+        <h3>Share Survey Link to Co-worker </h3>
+        <p>On your dashboard, you would see the link </p>
       </div>
-      <div>
-        <video autoPlay="true" loop="true" playsInline="" style={{ width: '100%', height: '400px' }}>
+
+      <div className="details-media">
+        <video autoPlay loop playsInline="">
           <source src={FeedbackVideo} type="video/mp4" />
         </video>
       </div>
-    </div>
+    </StyledDetailsArea>
   </div>
 );
 
