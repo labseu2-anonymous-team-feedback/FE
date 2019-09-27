bc53b5d3be5d53122ea7afc135aadd9d0ec3f907
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-09-27 10:00:25 +0100

    add media

--- src/components/landingpage/LandingPage.jsx
+++ src/components/landingpage/LandingPage.jsx
@@ -1,11 +1,23 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react';
 import styled from 'styled-components';
 import MainArea from './MainArea';
 import { white } from '../../styles/variables';
+import FeedbackVideo from '../../assets/media/feedback.mp4';
 
 const LandingPage = () => (
   <div>
     <MainArea />
+    <div>
+      <div>
+        <h1>Mee</h1>
+      </div>
+      <div>
+        <video autoPlay="true" loop="true" playsInline="" style={{ width: '100%', height: '400px' }}>
+          <source src={FeedbackVideo} type="video/mp4" />
+        </video>
+      </div>
+    </div>
   </div>
 );
 
--- src/components/landingpage/MainArea.jsx
+++ src/components/landingpage/MainArea.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import {
-  white, smallSpace, black, bodyHero, mediumSpace2, heading1,
+  white,
+  smallSpace,
+  black,
+  bodyHero,
+  mediumSpace2,
+  heading1
 } from '../../styles/variables';
 import Button from '../../styles/Button';
 
@@ -11,9 +16,11 @@ const MainArea = () => (
     <TextArea>
       <div className="text">
         <h1>Anonymous Team Feedback</h1>
-        <p>Improve team performance by giving and receiving feedback anonymously from team-mates</p>
+        <p>
+          Improve team performance by giving and receiving feedback anonymously
+          from team-mates
+        </p>
       </div>
-
 
       <div className="buttons-container ">
         <Link to="/register">
@@ -26,36 +33,36 @@ const MainArea = () => (
   </Wrapper>
 );
 
-
 const Wrapper = styled.div`
-min-height: 76vh;
-margin: 0 auto;
-max-width: 100%;
-padding: 0 ${smallSpace};
-display: flex;
-align-items: center;
-background: ${white};
+  min-height: 76vh;
+  margin: 0 auto;
+  max-width: 100%;
+  padding: 0 ${smallSpace};
+  display: flex;
+  align-items: center;
+  background: ${white};
 `;
 
 const TextArea = styled.div`
-position: relative;
-margin: 0 auto;
-height: 100%;
-width: 30%;
-font-size: 5rem;
-display: flex;
-flex-direction: column;
-justify-content: center;
+  position: relative;
+  margin: 0 auto;
+  height: 100%;
+  width: 30%;
+  font-size: 5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
-::before {
+  ::before {
     right: 100%;
-}
-::after {
+  }
+  ::after {
     left: 100%;
-}
+  }
 
-::before, ::after {
-    content: "";
+  ::before,
+  ::after {
+    content: '';
     display: block;
     background: url('../../assets/images/mobile.png');
     position: absolute;
@@ -64,9 +71,9 @@ justify-content: center;
     height: 6rem;
     top: 50%;
     margin: 0 15rem;
-}
+  }
 
-h1,
+  h1,
   p {
     color: ${black};
   }
@@ -81,10 +88,8 @@ h1,
     margin-bottom: ${mediumSpace2};
   }
   .text {
-    
     text-align: center;
   }
-
 
   .buttons-container {
     width: 60%;
@@ -94,7 +99,6 @@ h1,
       margin-right: ${smallSpace};
     }
   }
-
 `;
 
 export default MainArea;
